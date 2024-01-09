ccb4d7cfc66faaa14a33d82615cba36a2e85ca6c
“Marco <marco.pianaroli96@gmail.com>
2024-01-09 16:29:08 +0100

    styles table

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<!-- eslint-disable vue/attribute-hyphenation -->
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import DataTable from 'primevue/datatable'
@@ -20,14 +19,29 @@ onMounted(async () => {
 
 <template>
   <Suspense>
-    <div v-if="matches">
-      <DataTable :value="matches" show-grid-lines table-style=" min-width: 50rem" w-250 flex justify-center text-right>
-        <Column field="date" header="Date" />
-        <Column field="team1" header="Players team 1" />
-        <Column field="team2" header="Players team 2" />
-        <Column field="goals1" header="Goals team 1" />
-        <Column field="goals2" header="Goals team 2" />
-      </DataTable>
+    <div v-if="matches && matches.length > 0">
+      <table class="w[750px] border-collapse border border-gray-300">
+        <thead class="bg-gray-200">
+          <tr>
+            <th class="w-[25%]">Date</th>
+            <th class="w-[25%]">Team 1</th>
+            <th class="w-[25%]">Team 2</th>
+            <th class="w-[25%]">Goals Team 1</th>
+            <th class="w-[25%]">Goals Team 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="match in matches" :key="match.date" class="odd:bg-gray-100 even:bg-white">
+            <td class="w-[25%]">{{ match.date }}</td>
+            <td class="w-[25%]">{{ match.team1 }}</td>
+            <td class="w-[25%]">{{ match.team2 }}</td>
+            <td :class="{ 'text-green-500': match.goals1 > match.goals2, 'text-red-500': match.goals1 < match.goals2 }">{{
+              match.goals1 }}</td>
+            <td :class="{ 'text-green-500': match.goals2 > match.goals1, 'text-red-500': match.goals2 < match.goals1 }">{{
+              match.goals2 }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
     <div v-else>Loading...</div>
